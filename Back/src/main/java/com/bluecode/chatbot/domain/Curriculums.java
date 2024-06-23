415b17b03ba817967470bd09e06f5ea48d1e2462
jhan0121 <jhan0121@gmail.com>
2024-06-23 23:21:22 +0900

    feat: 커리큘럼 테스트용 데이터 생성 로직 추가
    커리큘럼 구조를 반영하여 DB에 저장하는 로직 추가

--- Back/src/main/java/com/bluecode/chatbot/domain/Curriculums.java
+++ Back/src/main/java/com/bluecode/chatbot/domain/Curriculums.java
@@ -2,12 +2,14 @@ package com.bluecode.chatbot.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@Setter
 public class Curriculums {
 
     // table id
