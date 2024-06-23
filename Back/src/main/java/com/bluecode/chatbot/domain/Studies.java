c6f22e7a231fd213a8c85fdab802c59923fab96a
jhan0121 <jhan0121@gmail.com>
2024-06-24 01:53:38 +0900

    feat: initDb tests, quiz, user, studies 데이터 추가
    quiz: 각 testable 챕터별 중급자*2, 초급자*1, 입문자*1문제씩 추가
    user: 초기테스트를 완료한 유저 데이터를 추가
    tests: 초기테스트 결과 데이터를 추가
    studies: 초기테스트 결과에 따른 학습자료 데이터를 추가

--- Back/src/main/java/com/bluecode/chatbot/domain/Studies.java
+++ Back/src/main/java/com/bluecode/chatbot/domain/Studies.java
@@ -2,11 +2,13 @@ package com.bluecode.chatbot.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
 @Getter
+@Setter
 public class Studies {
 
     // table id
