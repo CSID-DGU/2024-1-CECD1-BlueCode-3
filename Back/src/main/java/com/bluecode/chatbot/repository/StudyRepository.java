30957533a5a90a1b016441f043a93002ad5323c0
ewoo14 <ewoo2821@gmail.com>
2024-06-25 14:54:13 +0900

    Update StudyRepository.java

--- Back/src/main/java/com/bluecode/chatbot/repository/StudyRepository.java
+++ Back/src/main/java/com/bluecode/chatbot/repository/StudyRepository.java
@@ -4,10 +4,7 @@ import com.bluecode.chatbot.domain.Studies;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface StudyRepository extends JpaRepository<Studies, Long> {
-    List<Studies> findAllByUserIdAndCurriculumId(Long userId, Long curriculumId);
-    Studies findByUserIdAndCurriculumId(Long userId, Long curriculumId);
+public interface StudyRepository extends JpaRepository<Studies, Long>, StudyRepositoryCustom {
+
 }
