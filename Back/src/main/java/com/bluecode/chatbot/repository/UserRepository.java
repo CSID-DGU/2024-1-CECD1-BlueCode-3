487dad450dee4e29e027a4f6cc4874ca9d861b51
jhan0121 <jhan0121@gmail.com>
2024-06-26 19:30:13 +0900

    fix: UserRepository @Repository annotation 추가

--- Back/src/main/java/com/bluecode/chatbot/repository/UserRepository.java
+++ Back/src/main/java/com/bluecode/chatbot/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.bluecode.chatbot.repository;
 
 import com.bluecode.chatbot.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
 
     // 유저 테이블 id 기반 user 검색
