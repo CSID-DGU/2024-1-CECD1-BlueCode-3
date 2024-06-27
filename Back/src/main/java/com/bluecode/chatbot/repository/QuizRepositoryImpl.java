bb2a3218e3a5afaaeebbdd1d5dc69c67336713a4
jhan0121 <jhan0121@gmail.com>
2024-06-27 18:14:30 +0900

    feat: QuizRepository 쿼리 구현
    커리큘럼 id 기반 quiz 리스트 검색
    커리큘럼 id, level 기반 quiz 리스트 검색
    커리큘럼 id, 퀴즈 타입 기반 quiz 리스트 검색
    커리큘럼 id, 퀴즈 타입, level 기반 quiz 리스트 검색

--- Back/src/main/java/com/bluecode/chatbot/repository/QuizRepositoryImpl.java
+++ Back/src/main/java/com/bluecode/chatbot/repository/QuizRepositoryImpl.java
@@ -1,21 +1,11 @@
 package com.bluecode.chatbot.repository;
 
-import com.bluecode.chatbot.domain.Quiz;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 public class QuizRepositoryImpl implements QuizRepositoryCustom{
 
     private final EntityManager em;
 
-    @Override
-    public List<Quiz> findAllByChapNum(int chapterNum) {
-
-        return em.createQuery("select distinct q from Quiz q join fetch q.curriculum c where c.chapterNum = :chapNum", Quiz.class)
-                .setParameter("chapNum", chapterNum)
-                .getResultList();
-    }
 }
