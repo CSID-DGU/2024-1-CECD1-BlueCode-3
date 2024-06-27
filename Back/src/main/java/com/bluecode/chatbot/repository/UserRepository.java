ff19ad8918e56536f6118fb262d28e6fe98f995b
jhan0121 <jhan0121@gmail.com>
2024-06-27 18:11:55 +0900

    fix: StudyRepository, UserRepository 단일 리턴 쿼리에 대해 Optional을 리턴하도록 수정

--- Back/src/main/java/com/bluecode/chatbot/repository/UserRepository.java
+++ Back/src/main/java/com/bluecode/chatbot/repository/UserRepository.java
@@ -4,9 +4,14 @@ import com.bluecode.chatbot.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
 
     // 유저 테이블 id 기반 user 검색
-    Users findByUserId(Long userId);
+    Optional<Users> findByUserId(Long userId);
+
+    // 유저 로그인 id 기반 user 검색
+    Optional<Users> findByLoginId(String loginId);
 }
