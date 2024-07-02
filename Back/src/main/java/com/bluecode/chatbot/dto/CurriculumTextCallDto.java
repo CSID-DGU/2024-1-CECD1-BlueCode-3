523edd4058300f5c2108ea7fcc39bf2734844ad5
kivccc <71814049+kivccc@users.noreply.github.com>
2024-07-02 19:05:46 +0900

    feat : CurriculumTextCallDTO 추가

---
+++ Back/src/main/java/com/bluecode/chatbot/dto/CurriculumTextCallDto.java
@@ -0,0 +1,15 @@
+package com.bluecode.chatbot.dto;
+
+import com.bluecode.chatbot.domain.LevelType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CurriculumTextCallDto {
+    private Long userId;
+    private Long curriculumId;
+    private LevelType levelType;
+}
