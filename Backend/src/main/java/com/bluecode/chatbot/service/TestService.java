a775f8901aa9e0e641bfa142a6bd00fd6ea8d599
jhan0121 <jhan0121@gmail.com>
2024-07-08 23:07:08 +0900

    feat: TestService 구현
    초기 테스트 구성(crateInitTest)
    이해도 테스트 구성(crateNormalTest)
    답안 판정(submitAnswer)

--- Backend/src/main/java/com/bluecode/chatbot/service/TestService.java
+++ Backend/src/main/java/com/bluecode/chatbot/service/TestService.java
@@ -8,8 +8,6 @@ import com.bluecode.chatbot.repository.TestRepository;
 import com.bluecode.chatbot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -27,23 +25,27 @@ public class TestService {
     private final UserRepository userRepository;
     private final QuizRepository quizRepository;
 
-    // 초기 테스트용 문제 구성
-    public TestResponseDto forInitialTest(DataCallDto dto) throws Exception {
+    // 초기 테스트 구성
+    public TestResponseDto createInitTest(DataCallDto dto) throws Exception {
         log.info("forInitialTest with userId: {}, chapterNum: {}", dto.getUserId(), dto.getCurriculumId());
 
         Optional<Users> user = userRepository.findByUserId(dto.getUserId());
         Optional<Curriculums> chapter = curriculumRepository.findById(dto.getCurriculumId());
 
         if (user.isEmpty()) {
-            throw new IllegalStateException("유효하지 않은 유저 테이블 id 입니다.");
+            throw new IllegalArgumentException("유효하지 않은 유저 테이블 id 입니다.");
+        }
+
+        if (user.get().isInitTest()) {
+            throw new IllegalArgumentException("이미 초기 테스트를 진행하였습니다.");
         }
 
         if (chapter.isEmpty()) {
-            throw new IllegalStateException("유효하지 않은 커리큘럼 id 입니다.");
+            throw new IllegalArgumentException("유효하지 않은 커리큘럼 id 입니다.");
         }
 
         if (!chapter.get().isTestable()) {
-            throw new IllegalStateException("테스트를 진행하지 않는 챕터입니다.");
+            throw new IllegalArgumentException("테스트를 진행하지 않는 챕터입니다.");
         }
 
         // 각 난이도별로 퀴즈를 랜덤하게 선택
@@ -58,13 +60,81 @@ public class TestService {
         testQuizzes.add(easyQuizzes.get(0));
         testQuizzes.add(hardQuizzes.get(1));
 
-        // tests 테이블에 생성된 초기 테스트 현황 저장
+        List<Tests> tests = new ArrayList<>();
+
+        // tests 테이블에 생성된 초기 테스트 현황 생성 및 저장
         for (Quiz quiz : testQuizzes) {
             Tests test = new Tests();
             test.setUser(user.get());
             test.setQuiz(quiz);
             test.setPassed(false);
             test.setTestType(TestType.INIT);
+            test.setWrongCount(0);
+
+            testRepository.save(test);
+            tests.add(test);
+        }
+
+        TestResponseDto responseDto = new TestResponseDto();
+        List<TestResponseElementDto> testElements = new ArrayList<>();
+
+        for (int i = 0; i < tests.size(); i++) {
+
+            TestResponseElementDto element = new TestResponseElementDto();
+            element.setTestId(tests.get(i).getTestId());
+            element.setQuizId(testQuizzes.get(i).getQuizId());
+            element.setText(testQuizzes.get(i).getText());
+            element.setLevel(testQuizzes.get(i).getLevel());
+            element.setQuizType(testQuizzes.get(i).getQuizType());
+            element.setQ1(testQuizzes.get(i).getQ1());
+            element.setQ2(testQuizzes.get(i).getQ2());
+            element.setQ3(testQuizzes.get(i).getQ3());
+            element.setQ4(testQuizzes.get(i).getQ4());
+            testElements.add(element);
+        }
+
+        responseDto.setTests(testElements);
+
+        log.info("forInitialTest response: {}", responseDto);
+        return responseDto;
+    }
+
+    // 이해도 테스트 구성
+    public TestResponseDto createNormalTest(DataCallDto dto) throws Exception {
+
+        Optional<Users> user = userRepository.findByUserId(dto.getUserId());
+        Optional<Curriculums> chapter = curriculumRepository.findById(dto.getCurriculumId());
+
+        if (user.isEmpty()) {
+            throw new IllegalArgumentException("유효하지 않은 유저 테이블 id 입니다.");
+        }
+
+        if (chapter.isEmpty()) {
+            throw new IllegalArgumentException("유효하지 않은 커리큘럼 id 입니다.");
+        }
+
+        if (!chapter.get().isTestable()) {
+            throw new IllegalArgumentException("테스트를 진행하지 않는 챕터입니다.");
+        }
+
+        // 각 난이도별로 퀴즈를 랜덤하게 선택
+        List<Quiz> hardQuizzes = quizService.getRandomQuizzesByLevel(chapter.get(), QuizLevel.HARD, 1);
+        List<Quiz> normalQuizzes = quizService.getRandomQuizzesByLevel(chapter.get(), QuizLevel.NORMAL, 1);
+        List<Quiz> easyQuizzes = quizService.getRandomQuizzesByLevel(chapter.get(), QuizLevel.EASY, 1);
+
+        // EASY - NORMAL - HARD 순으로 문제셋 구성
+        List<Quiz> testQuizzes = new ArrayList<>();
+        testQuizzes.add(easyQuizzes.get(0));
+        testQuizzes.add(normalQuizzes.get(0));
+        testQuizzes.add(hardQuizzes.get(0));
+
+        // tests 테이블에 생성된 초기 테스트 현황 생성 및 저장
+        for (Quiz quiz : testQuizzes) {
+            Tests test = new Tests();
+            test.setUser(user.get());
+            test.setQuiz(quiz);
+            test.setPassed(false);
+            test.setTestType(TestType.NORMAL);
             test.setWrongCount(0);
 
             testRepository.save(test);
@@ -93,31 +163,35 @@ public class TestService {
         return responseDto;
     }
 
-    // 초기테스트용 답안 채점
+    // 답안 판정
     public TestAnswerResponseDto submitAnswer(TestAnswerCallDto dto) {
         log.info("submitAnswer with dto: {}", dto);
 
-        Optional<Quiz> quizOptional = quizRepository.findById(dto.getQuizId());
-        if (quizOptional.isEmpty()) {
-            throw new IllegalArgumentException("Invalid quiz ID: " + dto.getQuizId());
+        Optional<Users> user = userRepository.findById(dto.getUserId());
+        Optional<Tests> test = testRepository.findById(dto.getTestId());
+        Optional<Quiz> quiz = quizRepository.findById(dto.getQuizId());
+
+        if (quiz.isEmpty()) {
+            throw new IllegalArgumentException("유효하지 않은 quizId 입니다.");
         }
-        Quiz quiz = quizOptional.get();
 
-        Users user = userRepository.findById(dto.getUserId()).orElseThrow(() -> new IllegalArgumentException("Invalid user ID: " + dto.getUserId()));
+        if (user.isEmpty()) {
+            throw new IllegalArgumentException("유효하지 않은 userId 입니다.");
+        }
 
-        Tests test = new Tests();
-        test.setUser(user);
-        test.setQuiz(quiz);
+        if (test.isEmpty()) {
+            throw new IllegalArgumentException("유효하지 않은 testId 입니다.");
+        }
 
         // 사용자 답안 채점
-        boolean passed = quiz.getAnswer().equals(dto.getAnswer());
-        test.setPassed(passed);
+        boolean passed = quiz.get().getAnswer().equals(dto.getAnswer());
+        test.get().setPassed(passed);
 
         if (!passed) {
-            test.setWrongCount(test.getWrongCount() + 1);
+            test.get().setWrongCount(test.get().getWrongCount() + 1);
         }
 
-        testRepository.save(test);
+        testRepository.save(test.get());
 
         TestAnswerResponseDto responseDto = new TestAnswerResponseDto();
         responseDto.setPassed(passed);
