9efb831fca1651a5ac012404f1c92f5a476d29c3
kivccc <71814049+kivccc@users.noreply.github.com>
2024-07-13 01:42:12 +0900

    feat : UserService,TokenService 구현
    UserService -> findById() 유저 테이블 id 기반 user 검색
    TokenService  -> createNewAccessToken() access token 생성 서비스 코드

--- Backend/src/main/java/com/bluecode/chatbot/service/UserService.java
+++ Backend/src/main/java/com/bluecode/chatbot/service/UserService.java
@@ -1,56 +1,18 @@
 package com.bluecode.chatbot.service;
 
 import com.bluecode.chatbot.domain.Users;
-import com.bluecode.chatbot.dto.UserAddCallDto;
 import com.bluecode.chatbot.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     // 유저 테이블 id 기반 user 검색
     public Users findById(Long userId){
         return userRepository.findByUserId(userId)
                 .orElseThrow(()->new IllegalArgumentException("Unexpected user"));
-    }
-
-    // 유저 추가 (회원 가입)
-    @Transactional
-    public void addUser(UserAddCallDto userAddCallDto){
-        // 가입 DTO 넘어올때 중복 체크
-        if(userRepository.existsByUsername(userAddCallDto.getUsername())){
-            throw new IllegalArgumentException("Username already exists");
-        }
-        if (userRepository.existsByEmail(userAddCallDto.getEmail())) {
-            throw new IllegalArgumentException("Email already exists");
-        }
-        if (userRepository.existsById(userAddCallDto.getId())) {
-            throw new IllegalArgumentException("Id already exists");
-        }
-
-        // password는 bcrypt로 암호화하여 DB에 저장
-        String encodedPassword=bCryptPasswordEncoder.encode(userAddCallDto.getPassword());
-        Users user=Users.createUser(userAddCallDto.getUsername(),
-                userAddCallDto.getEmail(),
-                userAddCallDto.getId(),
-                encodedPassword,
-                userAddCallDto.getBirth(),
-                false);
-        userRepository.save(user);
-    }
-
-    public boolean checkUserNameDuplicate(String name){
-        return userRepository.existsByUsername(name);
-    }
-    public boolean checkEmailDuplicate(String email){
-        return userRepository.existsByEmail(email);
-    }
-    public boolean checkIdDuplicate(String id){
-        return userRepository.existsById(id);
     }
 }
