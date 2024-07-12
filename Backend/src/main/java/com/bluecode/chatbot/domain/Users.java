82fdd4a26705881dcad01e9ec8161f31004f61b8
kivccc <71814049+kivccc@users.noreply.github.com>
2024-07-13 01:00:26 +0900

    feat : Users 클래스 인증 객체 확장
    UserDetails 상속받도록 수정

--- Backend/src/main/java/com/bluecode/chatbot/domain/Users.java
+++ Backend/src/main/java/com/bluecode/chatbot/domain/Users.java
@@ -4,15 +4,19 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
-public class Users {
+public class Users implements UserDetails {
 
     // table id
     @Id @GeneratedValue
@@ -80,5 +84,30 @@ public class Users {
         user.setInitTest(initTest);
 
         return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
