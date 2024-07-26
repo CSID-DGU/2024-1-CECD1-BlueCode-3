fcf23ae01f0ce28ddce97a448e2d7ab307896702
jhan0121 <jhan0121@gmail.com>
2024-07-26 19:32:43 +0900

    feat: Mission 처리 구분용 Class 추가

--- Backend/src/main/java/com/bluecode/chatbot/domain/MissionConst.java
+++ Backend/src/main/java/com/bluecode/chatbot/domain/MissionConst.java
@@ -3,8 +3,13 @@ package com.bluecode.chatbot.domain;
 
 public class MissionConst {
 
+    private MissionConst() {
+    }
+
     public static final String TEST_SUBMIT = "TEST_SUBMIT";
+
     public static final String TEST_PASS = "TEST_PASS";
+
     public static final String TEST_FAIL = "TEST_FAIL";
 
     public static final String TEST_EASY_SUBMIT = "TEST_EASY_SUBMIT";
@@ -28,5 +33,17 @@ public class MissionConst {
     public static final String USER_LOGIN = "USER_LOGIN";
 
     public static final String USER_STREAK = "USER_STREAK";
+
+    public static final String MISSION_DAILY_COMPLETE = "MISSION_DAILY_COMPLETE";
+
+    public static final String MISSION_WEEKLY_COMPLETE = "MISSION_WEEKLY_COMPLETE";
+
+    public static String createConstByRootAndChapterName(Curriculums root, Curriculums chapter) {
+        return "STUDY_" + root.getCurriculumName().toUpperCase() + "_" + chapter.getChapterNum() + "_COMPLETE";
+    }
+
+    public static String createConstByRootName(Curriculums root) {
+        return "STUDY_" + root.getCurriculumName().toUpperCase() + "_COMPLETE";
+    }
 
 }
