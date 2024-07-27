950244160302436db62644d5caa0ca833ebd72d2
jhan0121 <jhan0121@gmail.com>
2024-07-27 18:37:31 +0900

    feat: chat 미션 키워드 추가

--- Backend/src/main/java/com/bluecode/chatbot/domain/MissionConst.java
+++ Backend/src/main/java/com/bluecode/chatbot/domain/MissionConst.java
@@ -6,6 +6,7 @@ public class MissionConst {
     private MissionConst() {
     }
 
+    // test 관련
     public static final String TEST_SUBMIT = "TEST_SUBMIT";
 
     public static final String TEST_PASS = "TEST_PASS";
@@ -26,15 +27,32 @@ public class MissionConst {
 
     public static final String TEST_INIT_COMPLETE = "TEST_INIT_COMPLETE";
 
-    public static final String STUDY_COMPLETE = "STUDY_COMPLETE";
-
+    // user 관련
     public static final String USER_LOGIN = "USER_LOGIN";
 
     public static final String USER_STREAK = "USER_STREAK";
 
+    // mission 관련
     public static final String MISSION_DAILY_COMPLETE = "MISSION_DAILY_COMPLETE";
 
     public static final String MISSION_WEEKLY_COMPLETE = "MISSION_WEEKLY_COMPLETE";
+
+    // chat 관련
+
+    public static final String CHAT_QUESTION  ="CHAT_QUESTION";
+
+    public static final String CHAT_DEF_QUESTION = "CHAT_DEF_QUESTION";
+
+    public static final String CHAT_CODE_QUESTION = "CHAT_CODE_QUESTION";
+
+    public static final String CHAT_ERRORS_QUESTION = "CHAT_ERRORS_QUESTION";
+
+    public static String createConstByQuestionTypeAndLevel(QuestionType questionType, int level) {
+        return "CHAT_" + questionType + "_UNTIL_" + level;
+    }
+
+    // study 관련
+    public static final String STUDY_COMPLETE = "STUDY_COMPLETE";
 
     public static String createConstByRootAndChapterName(Curriculums root, Curriculums chapter) {
         return "STUDY_" + root.getCurriculumName().toUpperCase() + "_" + chapter.getChapterNum() + "_COMPLETE";
