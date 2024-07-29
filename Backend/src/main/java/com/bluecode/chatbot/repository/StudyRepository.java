51e28ac82b9db2b0698869c06020e4afefeefae7
jhan0121 <jhan0121@gmail.com>
2024-07-29 20:43:19 +0900

    feat: StudyRepository 쿼리 추가 구현
    유저, 루크 커리큘럼 기반 Studies 리스트 검색
    유저, 루트 커리큘럼 기반 난이도가 EASY 인 Studies 리스트 검색
    유저, 챕터 기반 Studies 리스트 검색

--- Backend/src/main/java/com/bluecode/chatbot/repository/StudyRepository.java
+++ Backend/src/main/java/com/bluecode/chatbot/repository/StudyRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -25,4 +26,24 @@ public interface StudyRepository extends JpaRepository<Studies, Long>, StudyRepo
 
     // 유저, 커리큘럼(챕터), level 기반 Studies 단일 검색
     Optional<Studies> findByCurriculumAndUserAndLevel(Curriculums chapter, Users user, LevelType levelType);
+
+    // 유저, 루크 커리큘럼 기반 Studies 리스트 검색
+    @Query("select s from Studies s " +
+            "join fetch s.user " +
+            "join fetch s.curriculum " +
+            "where s.user = :user " +
+            "and s.curriculum.parent = :root")
+    List<Studies> findAllByUserAndRoot(@Param("user") Users user, @Param("root") Curriculums root);
+
+    // 유저, 루트 커리큘럼 기반 난이도가 EASY 인 Studies 리스트 검색
+    @Query("select s from Studies s " +
+            "join fetch s.user " +
+            "join fetch s.curriculum " +
+            "where s.user = :user " +
+            "and s.curriculum.parent = :root " +
+            "and s.level = com.bluecode.chatbot.domain.LevelType.EASY")
+    List<Studies> findAllEasyStudiesByUserAndRoot(@Param("user") Users user, @Param("root") Curriculums root);
+
+    // 유저, 챕터 기반 Studies 리스트 검색
+    List<Studies> findAllByUserAndCurriculum(Users user, Curriculums chapter);
 }
