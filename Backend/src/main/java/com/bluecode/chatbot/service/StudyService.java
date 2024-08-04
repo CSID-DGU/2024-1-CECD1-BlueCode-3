3a7d04136088c1c29a0b79f0365202384221aafb
jhan0121 <jhan0121@gmail.com>
2024-08-04 20:07:58 +0900

    fix: StudyService 재구성
    유저의 커리큘럼 학습 시작하기 위한 챕터 학습 데이터 생성 로직 변경
    textType에 따른 text 열 지정 및 생성 메서드 구현

--- Backend/src/main/java/com/bluecode/chatbot/service/StudyService.java
+++ Backend/src/main/java/com/bluecode/chatbot/service/StudyService.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -102,25 +101,29 @@ public class StudyService {
             throw new IllegalArgumentException("루트 커리큘럼 id가 아닙니다.");
         }
 
-        List<Studies> validation = studyRepository.findAllByUserAndRoot(user, root);
+        boolean validation = studyRepository.isExistByUserAndRoot(user, root);
 
         // 데이터 생성 여부 확인
-        if (!validation.isEmpty()) {
+        if (!validation) {
+            log.error("이미 해당 유저의 학습 대상 커리큘럼 데이터가 생성되어있습니다. user={}, root={}", user, root);
             throw new IllegalArgumentException("이미 해당 유저의 학습 대상 커리큘럼 데이터가 생성되어있습니다.");
         }
 
-        // 루트 커리큘럼 내 챕터 정보 검색
-        List<Curriculums> chapters = curriculumRepository.findAllByParentOrderByChapterNum(root);
+        // root 기반 자식 노드 전체 검색
+        List<Curriculums> child = curriculumRepository.findAllByRoot(root);
+
+        List<Curriculums> subChapters = child.stream().filter(i -> i.isLeafNode()).toList();
+
         List<Studies> studies = new ArrayList<>();
 
         // testable 조건에 따른 study 데이터 생성
-        for (Curriculums chapter : chapters) {
+        for (Curriculums chapter : subChapters) {
             if (chapter.isTestable()) {
-                studies.add(Studies.createStudy(user, chapter, false, null, LevelType.EASY));
-                studies.add(Studies.createStudy(user, chapter, false, null, LevelType.NORMAL));
-                studies.add(Studies.createStudy(user, chapter, false, null, LevelType.HARD));
+                studies.add(Studies.createStudy(user, chapter, false, null, null, null, LevelType.EASY));
+                studies.add(Studies.createStudy(user, chapter, false, null, null, null, LevelType.NORMAL));
+                studies.add(Studies.createStudy(user, chapter, false, null, null, null, LevelType.HARD));
             } else {
-                studies.add(Studies.createStudy(user, chapter, false, null, LevelType.EASY));
+                studies.add(Studies.createStudy(user, chapter, false, null, null, null, LevelType.EASY));
             }
         }
 
@@ -174,7 +177,7 @@ public class StudyService {
     }
 
     // 유저의 커리큘럼 학습 완료 처리
-    public String chapterPass(CurriculumPassCallDto dto) {
+    public String chapterPass(CurriculumPassCallDto dto) throws Exception {
 
         Optional<Users> userOptional = userRepository.findByUserId(dto.getUserId());
         Optional<Curriculums> chapterOptional = curriculumRepository.findById(dto.getCurriculumId());
@@ -195,7 +198,7 @@ public class StudyService {
             List<Studies> chapters = studyRepository.findAllByUserAndCurriculum(user, chapter);
 
             if (chapters.isEmpty() || chapters.size() < 3) {
-                log.error("학습중인 챕터가 아닙니다. size: {}", chapters.size());
+                log.error("학습중인 챕터가 아닙니다. chapters={}", chapters);
                 throw new IllegalArgumentException("학습중인 챕터가 아닙니다.");
             }
 
@@ -204,7 +207,6 @@ public class StudyService {
             Studies studyHard = chapters.stream().filter(s -> s.getLevel().equals(LevelType.HARD)).findFirst().get();
 
             if (dto.getCurrentLevel() == LevelType.HARD) {
-
                 if (dto.getNextLevel() == LevelType.HARD) {
                     studyHard.setPassed(true);
                     studyRepository.save(studyHard);
@@ -245,11 +247,11 @@ public class StudyService {
                 }
             }
 
-        } else {
+        } else if (!chapter.isTestable()) {
             List<Studies> studyValid = studyRepository.findAllByUserAndCurriculum(user, chapter);
 
             if (studyValid.isEmpty()) {
-                log.error("학습중인 챕터가 아닙니다.");
+                log.error("학습중인 챕터가 아닙니다. user={}, chapter={}", user, chapter);
                 throw new IllegalArgumentException("학습중인 챕터가 아닙니다.");
             }
 
@@ -258,10 +260,12 @@ public class StudyService {
             study.setPassed(true);
 
             studyRepository.save(study);
+        } else {
+            throw new Exception("유효하지 않은 chapter.testable");
         }
 
         // 특정 챕터 완료 미션 처리
-        eventPublisher.publishEvent(new UserActionEvent(this, user, ServiceType.STUDY, MissionConst.createConstByRootAndChapterName(chapter.getParent(), chapter)));
+        eventPublisher.publishEvent(new UserActionEvent(this, user, ServiceType.STUDY, MissionConst.createConstByRootAndSubChapterName(chapter.getParent(), chapter)));
 
         // 커리큘럼 학습 완료 미션 처리
         eventPublisher.publishEvent(new UserActionEvent(this, user, ServiceType.STUDY, MissionConst.STUDY_COMPLETE));
@@ -278,39 +282,77 @@ public class StudyService {
         Optional<Curriculums> chapterOptional = curriculumRepository.findById(dto.getCurriculumId());
 
         if (userOptional.isEmpty()) {
+            log.error("유효하지 않은 유저 테이블 id 입니다.");
             throw new IllegalArgumentException("유효하지 않은 유저 테이블 id 입니다.");
         }
 
         if (chapterOptional.isEmpty()) {
-            throw new IllegalArgumentException("유효하지 않은 루트 커리큘럼 id 입니다.");
+            log.error("유효하지 않은 커리큘럼 id 입니다.");
+            throw new IllegalArgumentException("유효하지 않은 커리큘럼 id 입니다.");
         }
 
         Users user = userOptional.get();
-        Curriculums chapter = chapterOptional.get();
+        Curriculums subChapter = chapterOptional.get();
 
-        Optional<Studies> studyOptional = studyRepository.findByCurriculumAndUserAndLevel(chapter, user, dto.getLevelType());
+        if (!subChapter.isLeafNode()) {
+            log.error("서브챕터 커리큘럼이 아닙니다. subChapter={}", subChapter);
+            throw new IllegalArgumentException("서브챕터 커리큘럼이 아닙니다.");
+        }
+
+        Optional<Studies> studyOptional = studyRepository.findByCurriculumAndUserAndLevel(subChapter, user, dto.getLevelType());
 
         if (studyOptional.isEmpty()) {
+            log.error("유효하지 않은 study 입니다.");
             throw new IllegalArgumentException("유효하지 않은 study 입니다.");
         }
 
         Studies study = studyOptional.get();
 
         // 학습 내용이 없으면 GPT API를 호출하여 학습 내용 생성
-        if (study.getText() == null) {
-            String keyword = curriculumService.getKeywordForLevel(chapter, dto.getLevelType());
-            String fullKeyword = chapter.getCurriculumName() + ": " + keyword;
-            String generatedResponse = requestGptText(fullKeyword, dto.getCurriculumId()); // 커리큘럼 ID로 루트 커리큘럼 이름을 조회
-            String generatedText = apiService.extractContentFromResponse(generatedResponse); // json 형식 응답을 text로 추출
-
-            study.setText(generatedText);
-        }
+        String text = createTextByTextType(study, dto.getTextType());
 
         StudyTextDto responseDto = new StudyTextDto();
-        responseDto.setText(study.getText());
+        responseDto.setText(text);
 
         log.info("getCurriculumText returning: {}", responseDto);
         return responseDto;
+    }
+
+    // textType에 따른 text 열 지정 및 생성
+    private String createTextByTextType(Studies study, TextType textType) {
+
+        if (textType == TextType.DEFS) {
+
+            if (study.getTextDef() == null) {
+                // TODO GPT API 로직 재구성 필요
+//                String fullKeyword = chapter.getCurriculumName() + ": " + keyword;
+//                String generatedResponse = requestGptText(fullKeyword, dto.getCurriculumId()); // 커리큘럼 ID로 루트 커리큘럼 이름을 조회
+//                String generatedText = apiService.extractContentFromResponse(generatedResponse); // json 형식 응답을 text로 추출
+//                study.setText(generatedText);
+                study.setTextDef("GPT 생성 text: 이론");
+            }
+            return study.getTextDef();
+
+        } else if (textType == TextType.CODE) {
+
+            if (study.getTextCode() == null) {
+                // TODO GPT API 로직 재구성 필요
+                study.setTextCode("GPT 생성 text: 실습");
+            }
+            return study.getTextDef();
+
+        } else if (textType == TextType.QUIZ) {
+
+            if (study.getTextQuiz() == null) {
+                // TODO GPT API 로직 재구성 필요
+                study.setTextQuiz("GPT 생성 text: 연습문제");
+            }
+            return study.getTextQuiz();
+
+        } else {
+            log.error("유효하지 않은 textType={}" ,textType);
+            throw new IllegalArgumentException("유효하지 않은 textType");
+        }
     }
 
     // GPT API를 호출하여 학습 내용 생성
