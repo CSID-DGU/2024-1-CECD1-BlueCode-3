5e99c4b6dc4eef18b43b94e45be7fc54a3476823
jhan0121 <jhan0121@gmail.com>
2024-08-04 20:05:21 +0900

    fix: StudyRepository 재구성
    유저, 커리큘럼(서브챕터), level 기반 Studies 단일 검색
    유저, 루트 커리큘럼 기반 Studies 리스트 검색
    유저, 루트 커리큘럼 기반 존재성 검색
    유저, 루트 커리큘럼 기반 난이도가 EASY 인 Studies 리스트 검색

--- Backend/src/main/java/com/bluecode/chatbot/repository/StudyRepository.java
+++ Backend/src/main/java/com/bluecode/chatbot/repository/StudyRepository.java
@@ -24,23 +24,32 @@ public interface StudyRepository extends JpaRepository<Studies, Long>, StudyRepo
             " and s.level = :levelType")
     Optional<Studies> findByUserIdAndCurriculumIdAndLevel(@Param("userId") Long userId, @Param("curriculumId") Long curriculumId, @Param("levelType") LevelType levelType);
 
-    // 유저, 커리큘럼(챕터), level 기반 Studies 단일 검색
+    // 유저, 커리큘럼(서브챕터), level 기반 Studies 단일 검색
     Optional<Studies> findByCurriculumAndUserAndLevel(Curriculums chapter, Users user, LevelType levelType);
 
-    // 유저, 루크 커리큘럼 기반 Studies 리스트 검색
+    // 유저, 루트 커리큘럼 기반 Studies 리스트 검색
     @Query("select s from Studies s " +
             "join fetch s.user " +
             "join fetch s.curriculum " +
             "where s.user = :user " +
-            "and s.curriculum.parent = :root")
+            "and s.curriculum.root = :root")
     List<Studies> findAllByUserAndRoot(@Param("user") Users user, @Param("root") Curriculums root);
+
+
+    // 유저, 루트 커리큘럼 기반 존재성 검색
+    @Query("select count(s.studyId) > 0 from Studies s " +
+            "join s.user " +
+            "join s.curriculum " +
+            "where s.user = :user " +
+            "and s.curriculum.root = :root")
+    boolean isExistByUserAndRoot(@Param("user") Users user, @Param("root") Curriculums root);
 
     // 유저, 루트 커리큘럼 기반 난이도가 EASY 인 Studies 리스트 검색
     @Query("select s from Studies s " +
             "join fetch s.user " +
             "join fetch s.curriculum " +
             "where s.user = :user " +
-            "and s.curriculum.parent = :root " +
+            "and s.curriculum.root = :root " +
             "and s.level = com.bluecode.chatbot.domain.LevelType.EASY")
     List<Studies> findAllEasyStudiesByUserAndRoot(@Param("user") Users user, @Param("root") Curriculums root);
 
