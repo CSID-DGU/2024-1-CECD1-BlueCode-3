ef1debf4cbe35d153bb1f123cd365033f4f9ea4a
jhan0121 <jhan0121@gmail.com>
2024-08-04 20:03:03 +0900

    fix: CurriculumRepository 쿼리 재구성
    루트 커리큘럼 기반 챕터 리스트 검색 + 챕터 커리큘럼 기반 서브 챕터 리스트 검색(chapterNum & subChapterNum 정렬)
    root 커리큘럼 기반 루트 파생 커리큘럼 리스트 검색
    루트 커리큘럼 리스트 검색

--- Backend/src/main/java/com/bluecode/chatbot/repository/CurriculumRepository.java
+++ Backend/src/main/java/com/bluecode/chatbot/repository/CurriculumRepository.java
@@ -2,13 +2,38 @@ package com.bluecode.chatbot.repository;
 
 import com.bluecode.chatbot.domain.Curriculums;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CurriculumRepository extends JpaRepository<Curriculums, Long>, CurriculumRepositoryCustom {
+public interface CurriculumRepository extends JpaRepository<Curriculums, Long> {
 
-    // 부모 커리큘럼 class 기반 커리큘럼 chapterNum 순서로 전체 챕터 커리큘럼 검색
-    List<Curriculums> findAllByParentOrderByChapterNum(Curriculums parent);
+    // 루트 커리큘럼 기반 챕터 리스트 검색 + 챕터 커리큘럼 기반 서브 챕터 리스트 검색(chapterNum & subChapterNum 정렬)
+    @Query("select c from Curriculums c " +
+            "join fetch c.root " +
+            "join fetch c.parent " +
+            "where c.parent = :parent " +
+            "order by c.chapterNum, c.subChapterNum")
+    List<Curriculums> findAllChildByParentOrderByChapterNumAndSubChapterNum(@Param("parent") Curriculums parent);
+
+
+    // root 커리큘럼 기반 루트 파생 커리큘럼 리스트 검색
+    List<Curriculums> findAllByRoot(Curriculums root);
+
+    // root 커리큘럼 기반 리프 노드 커리큘럼 리스트 검색
+    @Query("select c from Curriculums c " +
+            "join fetch c.root " +
+            "join fetch c.parent " +
+            "where c.root = :root " +
+            "and c.leafNode = :leafNode " +
+            "order by c.chapterNum asc, c.subChapterNum asc")
+    List<Curriculums> findAllByRootAndLeafNodeOrderByChapterNumAndSubChapterNum(Curriculums root, boolean leafNode);
+
+    // 루트 커리큘럼 리스트 검색
+    @Query("select c from Curriculums c " +
+            "where c.rootNode = true")
+    List<Curriculums> findAllRootCurriculumList();
 }
